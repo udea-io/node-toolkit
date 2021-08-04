1160b5c3ea53d25ccb3ede5ffc134b00e63cc2de
Kent <iamcxa@gmail.com>
2021-08-05 02:55:53 +0800

    feat: 🎸 add react-native config, remove not basic-usage package

---
+++ packages/eslint-config/react-native.js
@@ -0,0 +1,20 @@
+module.exports = {
+	extends: ['./index', './react', '@react-native-community', 'plugin:react-native/all'],
+
+	env: {
+		'react-native/react-native': true,
+		browser: true,
+		node: true,
+	},
+
+	settings: {
+		react: {
+			version: 'detect',
+		},
+	},
+
+	rules: {
+		'react-native/no-raw-text': 0, // Avoid false positive, wait for fix
+		'react-native/sort-styles': 0,
+	},
+};
